41cbc28de7976a1fa582defcee1e601e40429a14
flyguy-595 <williamuhlarik@gmail.com>
2019-12-04 12:28:56 -0600

    Final Build

--- app/src/main/java/edu/ilstu/cartelematics/IoT.java
+++ app/src/main/java/edu/ilstu/cartelematics/IoT.java
@@ -3,10 +3,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class IoT {
 
     private static AWSConnection connection;
     private static String[] data = {"", "", "", "", "", "", ""};
+    private static DecimalFormat decimalFormat = new DecimalFormat("0.00");
 
     public static void Connect(Context activityContext){
         connection = AWSConnection.newInstance(activityContext);
@@ -15,6 +18,14 @@ public class IoT {
 
     public static void setData(String[] AWSData){
         data = AWSData;
+        double fuelLevel = Double.parseDouble(data[1].substring(0, data[1].length()-8));
+        data[1] = "Fuel Level: " + decimalFormat.format(fuelLevel) + "%";
+        double rpm = Double.parseDouble(data[2].substring(0, data[2].length()-23));
+        data[2] = "RPMs: " +  rpm;
+        data[3] = "Speed: " + data[3];
+        double throttlePos = Double.parseDouble(data[4].substring(0, data[4].length()-8));
+        data[4] = "Throttle Pos: " + decimalFormat.format(throttlePos) + "%";
+        data[5] = "Engine Time: " + data[5].substring(0, data[5].length()-8) + "s";
     }
 
     public static String getMph(){
